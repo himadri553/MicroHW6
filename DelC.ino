5d831e1d2b08f58297c38be820a0b386427ca7c4
himadri553 <164025459+himadri553@users.noreply.github.com>
2024-04-10 18:17:49 -0400

    Update DelC.ino

--- DelC.ino
+++ DelC.ino
@@ -4,35 +4,39 @@
   once the button is turned on, wait and turn servo on to push the button off
   if possible, use a counter to make the machine more "mad" and swtich up servo behavior
 */
+#include <Servo.h>
+Servo servo;
 #define buttonPin 2
 #define LED_Pin 42
+#define servoPin 23
 int ledState = LOW;
 int madState = 1;
 bool mad_trigger = false;
 unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
+unsigned long debounceDelay = 100;
+int angle = 10;
+
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(LED_Pin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(buttonPin), initial_buttonISR, FALLING);
   Serial.begin(9600);
-
+  servo.attach(servoPin);
 }
 
 void loop() {
-  // vars
-
   // check if led is on, then get mad
   if (ledState == HIGH) {
     mad_trigger = true;
   }
   
   if (mad_trigger == true) {
-    mad_trigger = false; // cool off after
-    switch (madState) {
+    // select madstate
+    switch (madState) { // its not swtiching!!!
       case 1:
         madState1();
+        Serial.println("madState1");
         break;
       case 2:
         madState2();
@@ -41,6 +45,13 @@ void loop() {
         madState3();
         break;
     }
+    // increment madState
+    madState = madState + 1;
+    if (madState == 3) {
+      madState = 1;
+    }
+    // cool off after
+    mad_trigger = false;
   }
 }
 
@@ -52,17 +63,34 @@ void initial_buttonISR() {
     digitalWrite(LED_Pin, ledState);
   }
 }
-
 // servo actions when mad, will not be mad after
-void madState1() {
-  
+void madState1() { // somewhere here ig, after the led is turned off and 
+  delay(1000);
+  servo.write(0); 
+  delay(1000);
+  servo.write(90);
+  delay(1000);
+  servo.write(0);
+  delay(1000);
 }
 
 void madState2() {
-
+  delay(1500);
+  servo.write(0); 
+  delay(1000);
+  servo.write(90);
+  delay(1000);
+  servo.write(0);
+  delay(1000);
 }
 
 void madState3() {
-
+  delay(2000);
+  servo.write(0); 
+  delay(1000);
+  servo.write(90);
+  delay(1000);
+  servo.write(0);
+  delay(1000);
 }
 
